9698e5c4206376da95b4ebb7f94d102711f49428
Florin12er <apetreiflorin236@gmail.com>
2024-09-03 23:19:29 +0200

    added the project added inertia and added the models and controllers

---
+++ resources/js/Pages/Auth/Login.jsx
@@ -0,0 +1,105 @@
+import { Head, Link, useForm } from "@inertiajs/react";
+import GuestLayout from "@/Layouts/GuestLayout";
+import Checkbox from "@/Components/Checkbox";
+import InputError from "@/Components/InputError";
+import InputLabel from "@/Components/InputLabel";
+import PrimaryButton from "@/Components/PrimaryButton";
+import TextInput from "@/Components/TextInput";
+
+export default function Login({ status, canResetPassword }) {
+    const { data, setData, post, processing, errors, reset } = useForm({
+        email: "",
+        password: "",
+        remember: false,
+    });
+
+    const submit = (e) => {
+        e.preventDefault();
+        post(route("login"), {
+            onFinish: () => reset("password"),
+        });
+    };
+
+    return (
+        <GuestLayout>
+            <Head title="Log in" />
+
+            {status && (
+                <div className="mb-4 text-sm font-medium text-green-600">
+                    {status}
+                </div>
+            )}
+
+            <form onSubmit={submit} className="space-y-6">
+                <div>
+                    <InputLabel htmlFor="email" value="Email" />
+                    <TextInput
+                        id="email"
+                        type="email"
+                        name="email"
+                        value={data.email}
+                        className="mt-1 block w-full"
+                        autoComplete="username"
+                        isFocused={true}
+                        onChange={(e) => setData("email", e.target.value)}
+                    />
+                    <InputError message={errors.email} className="mt-2" />
+                </div>
+
+                <div>
+                    <InputLabel htmlFor="password" value="Password" />
+                    <TextInput
+                        id="password"
+                        type="password"
+                        name="password"
+                        value={data.password}
+                        className="mt-1 block w-full"
+                        autoComplete="current-password"
+                        onChange={(e) => setData("password", e.target.value)}
+                    />
+                    <InputError message={errors.password} className="mt-2" />
+                </div>
+
+                <div className="flex items-center justify-between">
+                    <label className="flex items-center">
+                        <Checkbox
+                            name="remember"
+                            checked={data.remember}
+                            onChange={(e) =>
+                                setData("remember", e.target.checked)
+                            }
+                        />
+                        <span className="ml-2 text-sm text-gray-600 dark:text-gray-400">
+                            Remember me
+                        </span>
+                    </label>
+
+                    {canResetPassword && (
+                        <Link
+                            href={route("password.request")}
+                            className="text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 underline rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                        >
+                            Forgot your password?
+                        </Link>
+                    )}
+                </div>
+
+                <div className="flex flex-col space-y-2">
+                    <Link
+                        href="/register"
+                        className="text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 underline rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                    >
+                        Don't have an account?
+                    </Link>
+
+                    <PrimaryButton
+                        disabled={processing}
+                        className="w-full justify-center"
+                    >
+                        Log in
+                    </PrimaryButton>
+                </div>
+            </form>
+        </GuestLayout>
+    );
+}
